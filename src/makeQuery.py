14f0d8f9b3eb8aece07235d21452543f0b3d8aa4
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2023-10-24 17:13:32 -0400

    update

--- src/makeQuery.py
+++ src/makeQuery.py
@@ -2,14 +2,19 @@
 # -*- coding: utf-8 -*-
 
 name_str = "Add Your Name Here"
-
-
+databaseFile_str = "TODO"
 
 print(
     f"""
       Hi, {name_str}! Please add all code from class
       for accessing and querying an SQL database
       using only Python programming.
+""")
+
+print(
+    f"""
+      The included database that is accessed
+      by this code: {databaseFile_str}
 """)
 
 TODO
